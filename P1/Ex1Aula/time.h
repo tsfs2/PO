66635ce77aaf80bc0954284d5a8052f5ec515cb4
Tiago Silva <114942219+tsfs2@users.noreply.github.com>
2023-02-23 18:48:59 +0000

    Create time.h

---
+++ P1/Ex1Aula/time.h
@@ -0,0 +1,12 @@
+    unsigned int horas,min,seg;
+public:
+    time();
+    time(unsigned int h, unsigned int m, unsigned int s);
+    void print();
+    void sum(time a, time b);
+
+};
+
+
+
+#endif //AULAPEX1_TIME_H
