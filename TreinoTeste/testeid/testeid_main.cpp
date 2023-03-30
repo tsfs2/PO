31436e16a727b6bf590498b8007f5ef1674d3353
Tiago Silva <114942219+tsfs2@users.noreply.github.com>
2023-03-30 23:23:58 +0100

    Create testeid_main.cpp

---
+++ TreinoTeste/testeid/testeid_main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "testeid.h"
+using namespace std;
+int main(){
+    Teste t1;
+    cout<<t1<<endl;
+    Teste t3(10,"1/1/2001");
+    cout<<t3<<endl;
+    cin>>t3;
+    cout<<t3;
+ }
